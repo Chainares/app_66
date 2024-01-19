d8f607898101fd90623246c3c9f70071b328456e
Chainares <110587221+Chainares@users.noreply.github.com>
2024-01-19 12:06:29 +0700

    app_8_f
    งานที่6

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
+<?php
+session_start(); 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Webboard-onii</title>
+    <title>Webboard-</title>
 </head>
 <body>
-    <h1 style="text-align: center;"></h1>
+    <h1 style="text-align: center;">ooooooooooKaKooooooo</h1>
     <hr>
     <form>
     หมวดหมู่: 
@@ -15,13 +19,27 @@
          <option value="general">General</option>>เรื่องทั่วไป</option>
          <option value="study">Study</option>
     </select>
-    <a href="login.html" style="float:right;">เข้าสู่ระบบ</a>
+    <?php
+      if (!isset($_SESSION['id'])){
+        echo "<a href= login.php  style='float:right';>เข้าสู่ระบบ</a>";
+
+      }else{
+        echo"<div style='float:right;'>
+              ผู้ใช้งานระบบ:$_SESSION[username]&nbsp;&nbsp;
+              <a href=logout.php> ออกจากระบบ</a>
+            </div> ";
+        echo"<br><a href=newpost.php>สร้างกระทุ้ใหม่</a>";
+            }
+    ?>
   </form> 
   <ul>
-
   <?php
       for($i=1;$i<=10;$i++){
-        echo "<li><a href='post.php?id=$i'>กระทู้ที่ $i</a></li>";
+        echo "<li><a href='post.php?id=$i'>กระทู้ที่ $i</a>";
+        if(isset($_SESSION['id']) &&$_SESSION['role']=='a'){ 
+          echo "&nbsp;&nbsp;<a href=delete.php?id=$i>ลบ</a>";
+          }
+        echo"</li>";
         }
   ?>  
   
--- register.php
+++ register.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -47,7 +48,7 @@
         </table>
         <br>
         <div style="text-align: center;">
-             <a href="index.html">กลับหน้าหลัก</a>
+             <a href="index.php">กลับหน้าหลัก</a>
         </div>
     </form>
 </body>
